318d1ce51620abf860aee492e495389c5f5c9645
OralbekUrusbekov <oralbekurusbekov2022@gmail.com>
2025-12-06 11:17:49 +0500

    I make Repository for  User and Permission  entitys

---
+++ src/main/java/kz/com/project/repository/PermissionRepository.java
@@ -0,0 +1,10 @@
+package kz.com.project.repository;
+
+import kz.com.project.model.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long> {
+    Permission findByName(String name);
+}

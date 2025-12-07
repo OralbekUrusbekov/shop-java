fb43b1589ad88f334654f4be3ebc1fd5ac71cb53
OralbekUrusbekov <oralbekurusbekov2022@gmail.com>
2025-12-07 16:13:30 +0500

    I create 2 model Cat and Order

---
+++ src/main/java/kz/com/project/model/Order.java
@@ -0,0 +1,28 @@
+package kz.com.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private Cat cat;
+
+    private LocalDateTime orderDate = LocalDateTime.now();
+}

7f08af6924f9ba413181a2c0118564b3a799ec93
OralbekUrusbekov <161113665+OralbekUrusbekov@users.noreply.github.com>
2025-12-17 17:30:09 +0500

    I create dto for all entitys in the shop service

--- src/main/java/kz/com/project/Dto/PaymentDTO.java
+++ src/main/java/kz/com/project/Dto/PaymentDTO.java
@@ -1,20 +1,18 @@
 package kz.com.project.Dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class OrderDTO {
+public class PaymentDTO {
+    private Long paymentId;
     private Long orderId;
-    private List<OrderItemDTO> items;
-    private Double totalPrice;
+    private Double amount;
     private String status;
-    private LocalDateTime createdAt;
+    private LocalDateTime paymentDate;
 }
